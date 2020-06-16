30d211dcda4d425e87739d14d0be87ea954dcec3
刘晗 <1727762240@qq.com>
2020-06-17 01:04:59 +0800

    finish register and login

---
+++ app/src/main/java/com/example/pilipili_android/fragment/RegisterFragment.java
@@ -0,0 +1,29 @@
+package com.example.pilipili_android.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pilipili_android.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RegisterFragment extends Fragment {
+
+    public RegisterFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+}
