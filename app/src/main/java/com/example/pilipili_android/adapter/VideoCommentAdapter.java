c37938813eda662f69145ef46cc212b5cfe60fe9
Administrator <784748497@qq.com>
2020-06-19 20:55:29 +0800

    [add] add comment list

---
+++ app/src/main/java/com/example/pilipili_android/adapter/VideoCommentAdapter.java
@@ -0,0 +1,109 @@
+package com.example.pilipili_android.adapter;
+
+import android.content.Context;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.pilipili_android.R;
+import java.util.List;
+
+
+public class VideoCommentAdapter extends RecyclerView.Adapter {
+    private List<List<SpannableString>> replays;
+    private List<String> comments;
+    private Context mContext;
+
+    public VideoCommentAdapter(List<String> comments, List<List<SpannableString>> replays) {
+        this.comments = comments;
+        this.replays = replays;
+    }
+
+
+    @Override
+    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        mContext = parent.getContext();
+        return new ItemViewHolder(LayoutInflater.from(parent.getContext()).
+                inflate(R.layout.item_comment_video, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        ItemViewHolder itemViewHolder = (ItemViewHolder) holder;
+        itemViewHolder.mUsername.setText("刘薪王分身");
+        itemViewHolder.mUserAvatar.setImageResource(R.drawable.bxx);
+        itemViewHolder.mCommentTime.setText("99999999");
+        itemViewHolder.mLike.setText("88888888");
+        itemViewHolder.mContent.setText("刘薪王的分身很多");
+        List<SpannableString> strings = replays.get(position);
+        if (strings == null) {
+            itemViewHolder.replays[0].setVisibility(View.GONE);
+            itemViewHolder.replays[1].setVisibility(View.GONE);
+            itemViewHolder.replays[2].setVisibility(View.GONE);
+            itemViewHolder.replays[3].setVisibility(View.GONE);
+            return;
+        }
+        for (int i = 0; i < strings.size() && i < 3; i++) {
+            itemViewHolder.replays[i].setVisibility(View.VISIBLE);
+            itemViewHolder.replays[i].setText(strings.get(i));
+        }
+        if (strings.size() > 3) {
+            SpannableString click = new SpannableString("更多评论");
+            click.setSpan(new ClickableSpan() {
+                @Override
+                public void onClick(@NonNull View widget) {
+                    Toast.makeText(mContext, "真是太强了", Toast.LENGTH_LONG).show();
+                }
+            }, 0, click.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            itemViewHolder.replays[3].setMovementMethod(LinkMovementMethod.getInstance());
+            itemViewHolder.replays[3].setText(click);
+            itemViewHolder.replays[3].setVisibility(View.VISIBLE);
+        } else {
+            itemViewHolder.replays[3].setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return replays.size();
+    }
+
+
+    public class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView mUserAvatar;
+        TextView mUsername;
+        TextView mCommentTime;
+        TextView mContent;
+        TextView mLike;
+        TextView[]replays = new TextView[4];
+
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+            mUserAvatar = itemView.findViewById(R.id.item_user_avatar);
+            mUsername = itemView.findViewById(R.id.item_user_name);
+            mCommentTime = itemView.findViewById(R.id.item_comment_time);
+            mContent = itemView.findViewById(R.id.item_comment_content);
+            mLike = itemView.findViewById(R.id.item_comment_like);
+            replays[0] = itemView.findViewById(R.id.replay_one);
+            replays[1] = itemView.findViewById(R.id.replay_two);
+            replays[2] = itemView.findViewById(R.id.replay_three);
+            replays[3] = itemView.findViewById(R.id.replay_total);
+        }
+    }
+
+}
+
---
+++ app/src/main/java/com/example/pilipili_android/fragment/CommentDetailsFragment.java
@@ -0,0 +1,30 @@
+package com.example.pilipili_android.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pilipili_android.R;
+
+public class CommentDetailsFragment extends Fragment {
+
+    public CommentDetailsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_comment_details, container, false);
+    }
+}
